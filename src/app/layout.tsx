69694e4f262c80ed81353a18b855a6527c686632
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-07 09:28:59 -0300

    add style for text

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <main className="">{children}</main>
+        <main className="h-full text-white">{children}</main>
       </body>
     </html>
   );
