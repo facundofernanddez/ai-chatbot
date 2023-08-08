d6f6cba9f83c8ea9addb5bf7abc6f3ffb33f9703
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-08 08:03:13 -0300

    add svg for computer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
               className="w-full"
             >
               {message.role === "user" ? (
-                <div className="flex items-center gap-x-2 ">
+                <div className="flex gap-x-2 ">
                   <div className="bg-gray-500 h-12 w-12 rounded-lg">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
@@ -39,17 +39,18 @@ export default function Home() {
                   </p>
                 </div>
               ) : (
-                <div className="flex items-center gap-x-2 ">
-                  <div className="bg-gray-500 h-12 w-12 rounded-lg">
+                <div className="flex gap-x-2 ">
+                  <div className="bg-teal-500 h-12 w-12 rounded-lg">
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
                       viewBox="0 0 24 24"
                       fill="currentColor"
                       className="w-full h-full text-white p-1"
                     >
+                      <path d="M16.5 7.5h-9v9h9v-9z" />
                       <path
                         fillRule="evenodd"
-                        d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
+                        d="M8.25 2.25A.75.75 0 019 3v.75h2.25V3a.75.75 0 011.5 0v.75H15V3a.75.75 0 011.5 0v.75h.75a3 3 0 013 3v.75H21A.75.75 0 0121 9h-.75v2.25H21a.75.75 0 010 1.5h-.75V15H21a.75.75 0 010 1.5h-.75v.75a3 3 0 01-3 3h-.75V21a.75.75 0 01-1.5 0v-.75h-2.25V21a.75.75 0 01-1.5 0v-.75H9V21a.75.75 0 01-1.5 0v-.75h-.75a3 3 0 01-3-3v-.75H3A.75.75 0 013 15h.75v-2.25H3a.75.75 0 010-1.5h.75V9H3a.75.75 0 010-1.5h.75v-.75a3 3 0 013-3h.75V3a.75.75 0 01.75-.75zM6 6.75A.75.75 0 016.75 6h10.5a.75.75 0 01.75.75v10.5a.75.75 0 01-.75.75H6.75a.75.75 0 01-.75-.75V6.75z"
                         clipRule="evenodd"
                       />
                     </svg>
