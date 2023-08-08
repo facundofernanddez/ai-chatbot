300690e87896a2323e3db93ccac42efba58393c3
Alejandro Facundo Fernandez <facundofernanddez@gmail.com>
2023-08-08 07:56:06 -0300

    fix error with message

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -10,12 +10,12 @@ const openai = new OpenAIApi(config);
 export const runtime = "edge";
 
 export async function POST(req: Request) {
-  const { message } = await req.json();
+  const { messages } = await req.json();
 
   const response = await openai.createChatCompletion({
     model: "gpt-3.5-turbo",
     stream: true,
-    messages: message,
+    messages: messages,
   });
 
   const stream = OpenAIStream(response);
